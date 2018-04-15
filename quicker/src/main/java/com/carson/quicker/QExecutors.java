eaaa5acf0112dc9f5395d4d77300ba70b68378e8
carson <981385016@qq.com>
2018-04-15 22:18:29 +0800

    release 1.0.5; 集成rxlifecycle有效避免内存泄露。

--- quicker/src/main/java/com/carson/quicker/QExecutors.java
+++ quicker/src/main/java/com/carson/quicker/QExecutors.java
@@ -13,24 +13,18 @@ import java.util.concurrent.Executors;
 
 public class QExecutors {
 
+
+    private final Executor mDiskIO = Executors.newSingleThreadExecutor();
+    private final MainThreadExecutor mMainThread = new MainThreadExecutor();
+
+    private QExecutors() {
+    }
+
     private static class QExecutorsHandler {
         private static final QExecutors instance = new QExecutors();
     }
 
-    private final Executor mDiskIO;
-    private final MainThreadExecutor mMainThread;
-
-    private QExecutors() {
-        this(Executors.newSingleThreadExecutor(), new MainThreadExecutor());
-    }
-
-    private QExecutors(Executor diskIO, MainThreadExecutor mainThread) {
-        this.mDiskIO = diskIO;
-        this.mMainThread = mainThread;
-    }
-
-
-    public static final QExecutors init() {
+    public static final QExecutors with() {
         return QExecutorsHandler.instance;
     }
 
@@ -43,7 +37,7 @@ public class QExecutors {
         return mMainThread;
     }
 
-    private static class MainThreadExecutor implements Executor {
+    private class MainThreadExecutor implements Executor {
         private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
 
         @Override
